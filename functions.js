3227bdc338e12e3aacc496c7501005163a8a02d3
Ratiu <ratiualexcristian@gmail.com>
2021-09-27 21:27:04 +0300

    Drawin tables using js

---
+++ functions.js
@@ -0,0 +1,19 @@
+function drawEmptyTable() {}
+    s="";
+    ['a','b','c','d','e','f','g','h'].forEach((e,i)=> {
+        //for (var i=1; i<=8; i++) {
+            s+=`<div class="row">`
+            for (var j=1; j<=8; j++) {
+                s +=`<div class='column ${j%2 === i%2 ? "a": "b"}'>${i},${j}</div>`   
+            }
+            s+=`</div>`
+        
+        //}    
+        document.getElementById("board-layout").innerHTML=s;
+    })
+
+
+drawEmptyTable();
+
+//document.querySelector('.column').innerHTML=`<div class="piece dark-piece"></div>`;
+
