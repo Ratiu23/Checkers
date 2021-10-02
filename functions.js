d262976d542de12ea3d12d2ecd57de84d04a6eb3
Ratiu <ratiualexcristian@gmail.com>
2021-10-02 13:32:24 +0300

    Test

--- functions.js
+++ functions.js
@@ -1,16 +1,15 @@
-function drawEmptyTable() {}
+function drawEmptyTable() {
     var s = "";
-    ['a','b','c','d','e','f','g','h'].forEach((e,i) => {
-        //for (var i=1; i<=8; i++) {
+    ['h','g','f','e','d','c','b','a'].forEach((e,i) => {
             s+=`<div class="row">`
             for (var j=1; j<=8; j++) {
                 s += `<div class='column ${j%2 === i%2 ? "a": "b"}'>${e},${j}</div>`   
             }
             s += `</div>`
-        //}    
-        
     })
     document.getElementById("board-layout").innerHTML=s;
+}
+
 drawEmptyTable();
 
 //document.querySelector('.column').innerHTML=`<div class="piece dark-piece"></div>`;
