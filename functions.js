17f9d915638ffd845280b896d05ca47d2c46678c
Ratiu <ratiualexcristian@gmail.com>
2021-10-04 21:47:15 +0300

    even shorter functions

--- functions.js
+++ functions.js
@@ -14,29 +14,37 @@ function drawEmptyTable() {
 
 function drawPieces() {
   drawEmptyTable();
-  const light = `<div class="piece light-piece"></div>`;
-  addPiece("#h8", "light");
-  addPiece("#h6", "light");
-  addPiece("#h4", "light");
-  addPiece("#h2", "light");
-  addPiece("#g7", "light");
-  addPiece("#g5", "light");
-  addPiece("#g3", "light");
-  addPiece("#g1", "light");
+  addLight("h8");
+  addLight("h6");
+  addLight("h4");
+  addLight("h2");
+  addLight("g7");
+  addLight("g5");
+  addLight("g3");
+  addLight("g1");
 
-  addPiece("#a7", "dark");
-  addPiece("#a5", "dark");
-  addPiece("#a3", "dark");
-  addPiece("#a1", "dark");
-  addPiece("#b8", "dark");
-  addPiece("#b6", "dark");
-  addPiece("#b4", "dark");
-  addPiece("#b2", "dark");
+  addDark("a7");
+  addDark("a5");
+  addDark("a3");
+  addDark("a1");
+  addDark("b8");
+  addDark("b6");
+  addDark("b4");
+  addDark("b2");
+  //fillBottom();
 }
 
 function addPiece(id, color) {
   const piece = `<div class="piece ${color}-piece"></div>`;
-  document.querySelector(id).innerHTML = piece;
+  document.querySelector("#" + id).innerHTML = piece;
+}
+
+function addLight(id) {
+  addPiece(id, "light");
+}
+
+function addDark(id) {
+  addPiece(id, "dark");
 }
 
 drawPieces();
